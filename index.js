538315b97083a3d8813a90552129325f1def50ea
Ivy Tanui <ivy.tanui@strathmore.edu>
2025-01-16 14:48:46 +0300

    Commit

--- index.js
+++ index.js
@@ -6,11 +6,26 @@ const tutorials = [
   'What is NaN and how Can we Check for it',
   'What is the difference between stopPropagation and preventDefault?',
   'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
+  'What is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  'what is JSONP?',
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = function() {
+  function callback(el, index) {
+    let elArray = el.split(" ");
+    let elArrayCapitalized = elArray.map((el1) => el1[0].toUpperCase() + el1.substring(1));
+    let tutorialsCapitalized = elArrayCapitalized.join(" ");
+    return tutorialsCapitalized;
+  }
+  let newArray = tutorials.map(callback);
+  return newArray;
 }
+
+
+
+
+
+
+
+
